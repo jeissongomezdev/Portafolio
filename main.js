09c483747d34ac9a1ba5b00d3d3d727f1fc019ad
Jeisson Gómez <jeissongomez@hotmail.es>
2022-04-03 15:33:55 -0500

    Se agrega script para envio de formulario al correo

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 /* Change Background Header */
 function scrollHeader() {
   const nav = document.getElementById("header");
-  // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
   if (this.scrollY >= 200) nav.classList.add("scroll-header");
   else nav.classList.remove("scroll-header");
 }
@@ -32,6 +31,7 @@ function linkAction() {
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /* Intro Landing Animation */
+
 gsap.from(".l-header > *", 1, {
   opacity: 0,
   delay: 0.5,
@@ -43,3 +43,25 @@ gsap.from(".home__data > *", 1, {
   delay: 0.5,
   stagger: 0.5,
 });
+
+/* Send Form */
+
+const $form = document.querySelector("#form");
+
+$form.addEventListener("submit", handleSubmit);
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  const form = new FormData(this);
+  const response = await fetch(this.action, {
+    method: this.method,
+    body: form,
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  if (response.ok) {
+    this.reset();
+    alert("Gracias por contactarme, te responderé pronto");
+  }
+}
