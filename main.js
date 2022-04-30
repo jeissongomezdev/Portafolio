b75c3250eb21c7237311abe7df9ba29423e3b140
Jeisson Gómez <jeissongomez@hotmail.es>
2022-04-30 16:48:35 -0500

    Se agrega Dark Mode :crescent_moon:

--- main.js
+++ main.js
@@ -44,6 +44,12 @@ gsap.from(".home__data > *", 1, {
   stagger: 0.5,
 });
 
+gsap.from(".home__scroll", 1, {
+  opacity: 0,
+  delay: 2,
+  stagger: 0.5,
+});
+
 /* Send Form */
 
 const $form = document.querySelector("#form");
@@ -65,3 +71,56 @@ async function handleSubmit(event) {
     alert("Gracias por contactarme, te responderé pronto");
   }
 }
+
+/* Scroll sections active link */
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
+/* Light and Dark Mode */
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "bx-sun";
+
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "bx-moon" : "bx-sun";
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "bx-moon" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
